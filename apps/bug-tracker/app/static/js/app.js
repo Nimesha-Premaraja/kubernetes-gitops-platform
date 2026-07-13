101d56900362ca13c1aab418dbc81c781a4aebfb
NimeshDil <nimeshadilshanpm@gmail.com>
2026-07-13 09:12:23 +0530

    update codes

--- apps/bug-tracker/app/static/js/app.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Bug Tracker App Loaded');
-});
-
-function confirmDelete(message = 'Are you sure?') {
-    return confirm(message);
-}
